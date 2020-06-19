9c38591d40d66513583bb8302ae9cdacf8c5fd58
Shivaot <shivatiwari757@gmail.com>
2020-06-19 12:13:56 +0530

    service discovery, Zuul gateway, ribbon load balancer added

---
+++ photoappapigateway/src/main/java/com/shivaot/photoappapigateway/PhotoappapigatewayApplication.java
@@ -0,0 +1,17 @@
+package com.shivaot.photoappapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class PhotoappapigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoappapigatewayApplication.class, args);
+    }
+
+}
